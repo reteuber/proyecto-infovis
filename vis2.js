7d236175d0014f914db328275547e7c1ec51a764
Re <rsteuber@uc.cl>
2023-11-18 21:13:27 -0300

    nuevas bdd

---
+++ vis2.js
@@ -0,0 +1,7 @@
+const SVG4 = d3.select("#vis-4").append("svg");
+const SVG5 = d3.select("#vis-5").append("svg");
+const SVG6 = d3.select("#vis-6").append("svg");
+
+const DATOS2 = "https://raw.githubusercontent.com/reteuber/Bases-datos-proyecto/main/educacion_superior.json"
+// ----------------------------------  VISUALIZACIÓN 2  --------------------------------------------
+// Descripción   -----------------------------------------------------------------------------------
---
+++ vis3.js
@@ -0,0 +1,10 @@
+const SVG7 = d3.select("#vis-7").append("svg");
+const SVG8 = d3.select("#vis-8").append("svg");
+const SVG9 = d3.select("#vis-9").append("svg");
+
+const DATOS3 = "https://raw.githubusercontent.com/reteuber/Bases-datos-proyecto/main/censo.json"
+
+const DATOS4 = "https://raw.githubusercontent.com/reteuber/Bases-datos-proyecto/main/investigadores.json"
+
+// ----------------------------------  VISUALIZACIÓN 3  --------------------------------------------
+// Descripción   -----------------------------------------------------------------------------------
